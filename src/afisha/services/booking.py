d3c5ddb8ecadfa9abf0fd9320c1023f309293d72
Alisa Ponikarovskaya <coloponik@gmail.com>
2026-07-29 18:12:57 +0400

    refactor: update booking status checks, API responce validation and restructure imports

--- src/afisha/services/booking.py
+++ src/afisha/services/booking.py
@@ -2,20 +2,43 @@ import asyncio
 import datetime
 import logging
 
-from afisha.application.dto import CheckoutResponse, CheckoutBooking, BookingRead, ProtectionQuote, \
-    PaymentQuote, BookingCreate
-from afisha.exceptions import SeatAlreadyReservedError, SeatsNotFoundError, PaymentUnavailableError
-from afisha.infrastracture.postgres.models import BookingStatus, EventSeat, SeatStatus, Booking
+from afisha.application.dto import (
+    CheckoutResponse,
+    CheckoutBooking,
+    BookingRead,
+    ProtectionQuote,
+    PaymentQuote,
+    BookingCreate
+)
+from afisha.exceptions import (
+    SeatAlreadyReservedError,
+    SeatsNotFoundError,
+    PaymentUnavailableError,
+    SeatAlreadySoldError
+)
+from afisha.infrastracture.api_connectors.internal.payment import PaymentConnector
+from afisha.infrastracture.api_connectors.internal.protection import ProtectionConnector
+from afisha.infrastracture.api_connectors.schemas import CalculatedPaymentData, \
+    CalculatedProtectionData
+from afisha.infrastracture.postgres.manager import DatabaseManager
+from afisha.infrastracture.postgres.models import BookingStatus, EventSeat, SeatStatus
 
 
 logger = logging.getLogger(__name__)
 
 
 class BookingService:
-    def __init__(self, db, payment_connector, protection_connector) -> None:
+    def __init__(
+            self,
+            db: DatabaseManager,
+            payment_connector: PaymentConnector,
+            protection_connector: ProtectionConnector,
+            booking_ttl: datetime.timedelta
+    ) -> None:
         self.db = db
         self.payment_connector = payment_connector
         self.protection_connector = protection_connector
+        self.booking_ttl = booking_ttl
 
     async def reserve(
             self,
@@ -25,24 +48,24 @@ class BookingService:
     ) -> CheckoutResponse:
         booking = await self.prepare_booking(event_id, payload.seat_ids, user_id)
 
-        task_payment = asyncio.create_task(self.payment_connector.get_commission(
+        coro_payment = self.payment_connector.get_commission(
             booking_id=booking.id,
             amount=booking.amount,
-            currency="USD"
-        ))
+            currency="RUB"
+        )
 
         event = await self.db.events.get_event(event_id)
 
-        task_protection = asyncio.create_task(self.protection_connector.get_protection_info(
+        coro_protection = self.protection_connector.get_protection_info(
             booking_id=booking.id,
             ticket_amount=booking.amount,
             event_category=event.category,
             event_starts_at=event.starts_at
-        ))
+        )
 
         payment, protection = await asyncio.gather(
-            task_payment,
-            task_protection,
+            asyncio.wait_for(coro_payment, timeout=3.0),
+            asyncio.wait_for(coro_protection, timeout=3.0),
             return_exceptions=True
         )
 
@@ -95,8 +118,7 @@ class BookingService:
             self.validate_event_seats(event_seats, seat_ids)
 
             amount = sum(seat.price for seat in event_seats)
-            reserved_until = datetime.datetime.now(datetime.UTC) + datetime.timedelta(
-                minutes=15)
+            reserved_until = datetime.datetime.now(datetime.UTC) + self.booking_ttl
 
             await db.event_seats.reserve_seats(
                 event_seats=event_seats,
@@ -126,11 +148,12 @@ class BookingService:
 
         for seat in event_seats:
             current_time = datetime.datetime.now(datetime.UTC)
-            if (
-                    seat.status != SeatStatus.available and
-                    seat.reserved_until > current_time
-            ):
+
+            if seat.is_reserved(current_time):
                 raise SeatAlreadyReservedError()
+
+            if seat.status == SeatStatus.sold:
+                raise SeatAlreadySoldError()
 
     def _build_checkout_booking(
             self,
@@ -151,7 +174,7 @@ class BookingService:
         )
 
     def _build_checkout_protection(self, protection) -> ProtectionQuote | None:
-        if protection:
+        if protection is not None:
             return ProtectionQuote(
                 available=protection.available,
                 price=protection.price,
@@ -162,7 +185,7 @@ class BookingService:
 
     def _validate_payment_response(
             self,
-            payment: PaymentQuote | Exception
+            payment: CalculatedPaymentData | BaseException
     ) -> PaymentQuote:
         if isinstance(payment, Exception):
             logger.error(
@@ -171,11 +194,16 @@ class BookingService:
             )
             raise PaymentUnavailableError() from payment
 
-        return payment
+        return PaymentQuote(
+            commission=payment.commission,
+            total=payment.total,
+            payment_methods=payment.payment_methods,
+            expires_at=payment.expires_at
+        )
 
     def _handle_protection_response(
             self,
-            protection: ProtectionQuote | Exception
+            protection: CalculatedProtectionData | BaseException
     ) -> ProtectionQuote | None:
         if isinstance(protection, Exception):
             logger.error(
@@ -183,4 +211,10 @@ class BookingService:
                 exc_info=protection
             )
             protection = None
-        return protection
+
+        return ProtectionQuote(
+            available=protection.available,
+            price=protection.price,
+            covered_amount=protection.covered_amount,
+            description=protection.description
+        )
