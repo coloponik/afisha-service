9870cb06de6cb9b3a2ed42aae9f6846d128f30c8
Alisa Ponikarovskaya <coloponik@gmail.com>
2026-09-05 19:28:13 +0400

    feat: add explicit acknowledge type

--- src/afisha/infrastructure/tasks/taskiq_tasks.py
+++ src/afisha/infrastructure/tasks/taskiq_tasks.py
@@ -13,18 +13,16 @@ logger = logging.getLogger(__name__)
 
 @broker_cpu.task(
     task_name="generate_event_pdf_report",
+    ack_type="when_executed",
     retry_on_error=True,
     max_retries=2
 )
 async def generate_event_pdf_report(report_id: str) -> None:
     container = create_container(settings)
-    print("Container created")
     logger.info("Report started")
     async with container(scope=Scope.REQUEST) as request_container:
         service = await request_container.get(ReportService)
-        print("Container gotten")
         await service.generate_event_report(report_id)
-    print("Task finished")
     logger.info("Report finished")
 
 
